d1f92fada11e8b62bc38467c761ca32a7becf649
sushantn145 <67630542+sushantn145@users.noreply.github.com>
2020-10-06 19:27:40 +0530

    Delete p1.java

--- p1.java
+++
@@ -1,6 +0,0 @@
-//01.	Write a program to print Hello World. Compile and run it using command prompt
-public class Hello{
-	public static void main(Stiing.. args){
-		System.out.println("Hello Wolrd");
-	}
-}
